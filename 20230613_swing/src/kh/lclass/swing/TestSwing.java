b3f511558310335a29c854c1fe73f6da72d31a03
HDK <hdk8572@gmail.com>
2023-06-13 15:08:35 +0900

    0613_1500

---
+++ 20230613_swing/src/kh/lclass/swing/TestSwing.java
@@ -0,0 +1,16 @@
+package kh.lclass.swing;
+
+import javax.swing.JFrame;
+
+public class TestSwing extends JFrame{
+
+	public TestSwing() {
+
+		setTitle("Hello Window 프레임");
+		setSize(300, 300);
+		setVisible(true);
+	}
+	public static void main(String[] args) {
+		TestSwing frame = new TestSwing();
+	}
+}
