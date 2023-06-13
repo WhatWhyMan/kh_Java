ea6ad113cf0fe42e1ed228de15e1f87f4098e554
HDK <hdk8572@gmail.com>
2023-06-13 09:37:30 +0900

    0613 수정본

--- 20230609_tcp_client/src/kh/lclass/tcp/ClientMain.java
+++
@@ -1,9 +0,0 @@
-package kh.lclass.tcp;
-
-public class ClientMain {
-	public static void main(String[] args) {
-		// "127.0.0.1" 은 "localhost" = loopback
-		new TcpClient().testTcpClient("127.0.0.1",9001);
-	}
-
-}
--- 20230609_tcp_server/src/kh/lclass/tcp/ServerMain.java
+++
@@ -1,7 +0,0 @@
-package kh.lclass.tcp;
-
-public class ServerMain {
-	public static void main(String[] args) {
-		new TcpServer().testTcpServer(9001); // 1
-	}
-}
